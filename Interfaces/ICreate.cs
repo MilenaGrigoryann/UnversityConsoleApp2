f6a9b14e3a49fe327ee5d3376c53c1c859b910bc
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-18 19:30:47 +0400

    Add files via upload

---
+++ Interfaces/ICreate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UniversityConsoleApp.Models;
+
+namespace UniversityConsoleApp.Interfaces
+{
+    public interface ICreate
+    {
+        Person Create(string firstName, string lastName, int age);
+        Person[] Create(int count, int minAge);
+    }
+}
---
+++ Interfaces/IPrint.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UniversityConsoleApp.Models;
+
+namespace UniversityConsoleApp.Interfaces
+{
+    public interface IPrint
+    {
+        void Print(Person person);
+        void Print(Person[] people);
+    }
+}
