f8a7cf275c5dd69b192fac58ce6a1ceaac892719
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-18 19:32:02 +0400

    Add files via upload

---
+++ Program .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UniversityConsoleApp.Models;
+using UniversityConsoleApp.BL;
+using UniversityConsoleApp.Interfaces;
+
+namespace UniversityConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentManager studentManager = new StudentManager();
+            TeacherManager teacherManager = new TeacherManager();
+            Person[] students = studentManager.Create(19, 18);
+            Person[] teachers = teacherManager.Create(30, 19);
+            Person student = studentManager.Create("John", "Doe", 18);
+            Person teacher = teacherManager.Create("Ben", "Devis", 45);
+            UniversityManager.SwapWithStudents((Teacher)teacher, (Student[])students);
+            teacherManager.Print(teacher);
+            UniversityManager.SwapWithTeacher((Student)student, (Teacher)teacher);
+            studentManager.Print(student);
+            UniversityManager.SwapWithStudent((Teacher[])teachers, (Student[])students);
+            teacherManager.Print(teachers);
+            UniversityManager.SwapWithTeachers((Student[])students, (Teacher[])teachers);
+            studentManager.Print(students);
+            Group group = GroupManager.Create("C#");
+            Group[] groups = GroupManager.Create(5);
+            GroupManager.Print(groups);
+            students = UniversityManager.SwapWithGroup((Student[])students, group);
+            studentManager.Print(students);
+            Console.ReadKey();
+        }
+    }
+}
